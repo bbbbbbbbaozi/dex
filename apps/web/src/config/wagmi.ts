293ada50ae71d9bdcf7e3feb06c133371a339b62
baozi <baozi.cpp@gmail.com>
2026-06-04 18:26:39 +0800

    web/feat/wallet: 处理没有钱包ID时要主动报错

--- apps/web/src/config/wagmi.ts
+++ apps/web/src/config/wagmi.ts
@@ -1,9 +1,12 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { supportChains } from "@/config/chains";
 
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
+if (!projectId) throw new Error("Missing NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID");
+
 export const wagmiConfig = getDefaultConfig({
   appName: "dex",
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+  projectId,
   chains: supportChains,
   ssr: true,
 });
